35b022751a7c333695ec0a0f184e2290d4543c0c
Dominick Girard <dominickgirard112@gmail.com>
2024-06-04 19:46:51 -0400

    Update PipeMove.cs

--- Assets/Scripts/PipeMove.cs
+++ Assets/Scripts/PipeMove.cs
@@ -7,7 +7,6 @@ public class PipeMove : MonoBehaviour
 {
     public float moveSpeed = 6;
     public float deadZone = -45;
-
     void FixedUpdate()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
